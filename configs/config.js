fe51b66d404ce9c5659cae52f19efe981fd274c6
Unknown <yuchiehchen@outlook.com>
2018-10-17 14:43:28 +0800

    加入環境變數

--- configs/config.js
+++ configs/config.js
@@ -1,7 +1,7 @@
 const config = {
     mongodb: {
-        url: 'mongodb://localhost:27017',
-        dbName: 'myproject',
+        url: process.env.MONGODB_URL || 'mongodb://localhost:27017',
+        dbName: process.env.MONGODB_DB_NAME || 'myproject',
     }
 }
 module.exports = config;
